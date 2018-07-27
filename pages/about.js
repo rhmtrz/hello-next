0af691be036232446f0a1d20b36d85ebf55996b5
rhmtrz <2249301106@qq.com>
2018-07-27 13:15:40 +0900

    page and component

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+
+import Layout from '../src/component/my-layout'; 
+
+const linkStyle = {
+  marginRight: 15
+}
+
+const About = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>This is About page</h1>
+      </Layout>
+    </div>
+  )
+}
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Layout from '../src/component/my-layout'; 
+
+const Index = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Hello Next.js</h1>
+      </Layout>
+    </div>
+  )
+}
+
+export default Index;
